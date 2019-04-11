68f1e279744d76c993064b9347157addd4c849c3
Jeremy Florence <jeremy.r.florence@gmail.com>
2019-04-11 11:49:22 -0400

    Add docstrings to forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,12 @@
+'''Forms for the site'''
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
+    '''Form to login to the site'''
+
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
