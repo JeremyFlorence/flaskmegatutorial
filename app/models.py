f1b11272dbc893b254214d946f4c426b8c70be82
Jeremy Florence <jeremy.r.florence@gmail.com>
2019-04-11 14:09:51 -0400

    Implement set_password and check_password methods for User model

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@
 All tables for database defined here
 '''
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 
 
@@ -12,6 +13,14 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def set_password(self, password):
+        '''Sets the password for the user'''
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        '''Checks that the entered password is correct'''
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
