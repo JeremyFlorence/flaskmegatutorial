e7b94158e0b965e245c8a2402c0b5073836bc6ce
Jeremy Florence <jeremy.r.florence@gmail.com>
2019-04-12 13:52:03 -0400

    Fix a type in register template, forgot db import in routes.py

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
-from app import app
+from app import app, db
 from app.models import User
 from app.forms import LoginForm, RegistrationForm
 
